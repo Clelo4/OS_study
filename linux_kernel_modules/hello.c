219b0bbf97ee51411fa57588d741db62832926f8
jack <chengjunjie.jack@gmail.com>
2021-12-12 04:15:54 +0800

    add a example for linux kernel modules.

--- linux_kernel_modules/hello.c
+++ linux_kernel_modules/hello.c
@@ -1,7 +1,25 @@
 #include <linux/module.h>
 #include <linux/printk.h>
 
-int hello(void) {
-	
+/* This function is called when the module is loaded. */
+int hello(void) 
+{
+        printk(KERN_INFO "Loading module... Hello World!\n");
+
+        return 0;
 }
 
+/* This function is called when the module is removed. */
+void goodbye(void) 
+{
+        printk(KERN_INFO "Removing module... Goodbye World!\n");
+}
+
+/* Macros for registering module entry and exit points */
+module_init( hello );
+module_exit( goodbye );
+
+/* Macros for declaring module metadata */
+MODULE_DESCRIPTION("A basic Hello World module");
+MODULE_AUTHOR("Jack");
+MODULE_LICENSE("GPL");
