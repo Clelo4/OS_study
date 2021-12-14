ff926b0a47b2666414285c7948b0a034bba462d3
chengjunjie <chengjunjie.jack@gmail.com>
2021-12-14 10:09:14 +0800

    format code

--- linux_kernel_modules/hello.c
+++ linux_kernel_modules/hello.c
@@ -2,22 +2,18 @@
 #include <linux/printk.h>
 
 /* This function is called when the module is loaded. */
-int hello(void) 
-{
-        printk(KERN_INFO "Loading module... Hello World!\n");
+int hello(void) {
+  printk(KERN_INFO "Loading module... Hello World!\n");
 
-        return 0;
+  return 0;
 }
 
 /* This function is called when the module is removed. */
-void goodbye(void) 
-{
-        printk(KERN_INFO "Removing module... Goodbye World!\n");
-}
+void goodbye(void) { printk(KERN_INFO "Removing module... Goodbye World!\n"); }
 
 /* Macros for registering module entry and exit points */
-module_init( hello );
-module_exit( goodbye );
+module_init(hello);
+module_exit(goodbye);
 
 /* Macros for declaring module metadata */
 MODULE_DESCRIPTION("A basic Hello World module");
--- linux_task_list/main.c
+++ linux_task_list/main.c
@@ -8,35 +8,35 @@
  * @copyright Copyright (c) 2021
  */
 
+#include <linux/list.h>
 #include <linux/module.h>
 #include <linux/printk.h>
 #include <linux/sched.h>
 #include <linux/sched/task.h>
-#include <linux/list.h>
 
 extern struct task_struct init_task;
 
 void print_task(struct task_struct *, int);
 
 void print_task(struct task_struct *task, int offset) {
-	struct list_head *bro = &task->children;
-	struct list_head *posi;
-	list_for_each(posi, bro) {
-		typeof(struct task_struct *) entry = list_entry(posi, struct task_struct, sibling);
-		printk("%*s\\_ [%d] %s\n", offset, " ", entry->pid, entry->comm);
-		print_task(entry, offset + 4);
-	}
+  struct list_head *bro = &task->children;
+  struct list_head *posi;
+  list_for_each(posi, bro) {
+    typeof(struct task_struct *) entry =
+        list_entry(posi, struct task_struct, sibling);
+    printk("%*s\\_ [%d] %s\n", offset, " ", entry->pid, entry->comm);
+    print_task(entry, offset + 4);
+  }
 }
 
 int m_init(void) {
-	printk("Loading Module\n");
-	printk("-- [%d] %s", init_task.pid, init_task.comm);
-	print_task(&init_task, 4); 
-	return 0;
+  printk("Loading Module\n");
+  printk("-- [%d] %s", init_task.pid, init_task.comm);
+  print_task(&init_task, 4);
+  return 0;
 }
 
-void m_exit(void) {
-}
+void m_exit(void) {}
 
 module_init(m_init);
 module_exit(m_exit);
@@ -44,4 +44,3 @@ module_exit(m_exit);
 MODULE_DESCRIPTION("A basic module for study");
 MODULE_AUTHOR("Jack");
 MODULE_LICENSE("GPL");
-
