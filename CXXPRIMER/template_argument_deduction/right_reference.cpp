197c879d75764c8147e3561f683f905e345903ec
chengjunjie <chengjunjie.jack@gmail.com>
2022-01-14 05:30:54 -0500

    update

--- CXXPRIMER/template_argument_deduction/right_reference.cpp
+++ CXXPRIMER/template_argument_deduction/right_reference.cpp
@@ -22,6 +22,7 @@ int main() {
   // fcn1传入右值，模版参数T被推断为int类型
   fcn1(12);
   fcn1(cli * 12);
+  fcn3(cli * 12);
 
   // fcn1传入左值引用，T& &&会折叠成类型T&
   fcn1(li);
@@ -31,6 +32,9 @@ int main() {
   // 将左值传递给函数fcn1的右值引用形参，\且此右值引用形参指向模版类型参数时\
   编译器推断模版类型参数为实参的左值引用类型。这里模版参数T的类型为int&
   fcn1(i);
+  // i被赋值后，返回左值引用
+  fcn1(i = cli);
+  decltype(i = cli) a = i;  // a的类型是int&
 
   fcn1(std::move(ri));
   fcn3(ri);
