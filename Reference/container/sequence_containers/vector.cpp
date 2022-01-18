e7cbe372f7ced9546132b72b9b60196dee4006f2
chengjunjie <chengjunjie.jack@gmail.com>
2022-01-18 08:24:44 -0500

    update

--- Reference/container/sequence_containers/vector.cpp
+++ Reference/container/sequence_containers/vector.cpp
@@ -26,6 +26,23 @@ using vector_t = vector<int>;
 
 int main() {
   vector_t t = {1, 2, 3, 4, 4, 5};
+  // at()与operator[]的区别是：at()会进行边界检查，超出边界会跑出out_of_range异常
   assert(t.at(0) == t.operator[](0));
+  t.resize(1);
+  cout << "size: " << t.size() << endl;
+  cout << "capacity: " << t.capacity() << endl;
+  t.shrink_to_fit();
+  cout << "capacity: " << t.capacity() << endl;
+  // 用新内容替换原有的全部内容，同时也会调整容器大小
+  t.assign({1, 2, 3, 4, 5, 6, 7, 8, 9, 0});
+  cout << "capacity: " << t.capacity() << endl;
+  t.pop_back();
+  t.push_back(12);
+  cout << "capacity: " << t.capacity() << endl;
+  // 原本capacity：10，增加一个元素后，容量翻倍
+  t.push_back(11);
+  cout << "capacity: " << t.capacity() << endl;
+  t.erase(t.end()--);
+  cout << "capacity: " << t.capacity() << endl;
   return 0;
 }
