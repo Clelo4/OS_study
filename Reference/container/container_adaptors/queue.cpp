12d2c1034aa031656a0ba61da1982dca3e60228b
chengjunjie <chengjunjie.jack@gmail.com>
2022-01-19 01:11:38 -0500

    add reference/container/container_adaptors

---
+++ Reference/container/container_adaptors/queue.cpp
@@ -0,0 +1,17 @@
+/**
+ * @file queue.cpp
+ * @author your name (you@domain.com)
+ * @brief
+ * @version 0.1
+ * @date 2022-01-18
+ *
+ * @copyright Copyright (c) 2022
+ *
+ */
+
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+int main() { priority_queue return 0; }
