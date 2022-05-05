ad24b93d6ae5a244386fef64dab2cdc4665f7a8d
chengjunjie <chengjunjie.jack@gmail.com>
2022-05-06 02:00:22 +0800

    Update ContestHunter

---
+++ ContestHunter/main.cpp
@@ -0,0 +1,40 @@
+#include <algorithm>
+#include <cmath>
+#include <cstdio>
+#include <cstring>
+#include <iostream>
+#include <vector>
+
+#define MAX 201
+#define MOD 998244353
+#define EPS 1e-6
+#define pb push_back
+#define fi first
+#define se second
+#define mapa make_pair
+using T = long int;
+#define pii pair<T, T>
+
+using LI = long int;
+using LLI = long long int;  // c++11
+using ULLI = unsigned long long int;
+using UI = unsigned int;
+constexpr int INF = 0x3f3f3f3f;
+
+using namespace std;
+
+int main() {
+#ifdef DEBUG
+  freopen("./input", "r", stdin);
+  clock_t start = clock();
+#endif
+  std::ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+  cout.tie(nullptr);
+
+#ifdef DEBUG
+  clock_t end = clock();
+  printf("Time: %fms\n", double(end - start) / CLOCKS_PER_SEC * 1000);
+#endif
+  return 0;
+}
